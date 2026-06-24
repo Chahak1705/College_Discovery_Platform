9c3672f4de71eecf0cb94fd008ef1696d53523d2
Chahak <Chahak1705@users.noreply.github.com>
2026-06-24 13:06:12 +0530

    fix: reset next config structure and use script level build flags

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    // TypeScript type-checking errors ki wajah se production build fail hone se rokega
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Build ke waqt ESLint warnings ko bypass karega taaki code smoothly deploy ho jaye
-    ignoreDuringBuilds: true,
-  }
+  /* config options here */
 };
 
 export default nextConfig;
